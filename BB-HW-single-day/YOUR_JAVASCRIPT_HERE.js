509713200042b4440e4c7a3abf52d429cf52991e
Sjoerd Ligtvoet <sjoerd_ligtvoet@hotmail.com>
2018-07-06 22:40:39 +0200

    firs commit

---
+++ BB-HW-single-day/YOUR_JAVASCRIPT_HERE.js
@@ -0,0 +1,28 @@
+// Write your JS here
+
+
+let hero = {
+    name: "Ironman",
+    heroic: true,
+    inventory: [],
+    health: 10,
+    weapon: {type: "", damage: 2}
+
+
+}
+
+function rest(hero) {
+    hero.health = 10;
+    return hero;
+
+}
+
+function pickUpItem() {
+
+}
+
+function equipWeapon() {
+
+}
+
+let imageInn = document.querySelector()
